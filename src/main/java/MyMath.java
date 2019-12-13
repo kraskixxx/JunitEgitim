5aa720b521df0de0b7ced4a605998f55ba3fa7b0
kraskixxx <samet_163@hotmail.com>
2019-12-13 09:38:03 +0300

    isPrime metodu refactor edildi.

--- src/main/java/MyMath.java
+++ src/main/java/MyMath.java
@@ -3,28 +3,19 @@
  * @since
  */
 public class MyMath {
-
-    /**
-     * Method to check is input integer is prime or not
-     * @param number is integer
-     * @return true if the input integer is prime, and false if not.
-     */
     public static boolean isPrime(int number) {
-        if(number >= 2) {
-            if(number %2 == 0) {
-                if( number == 2) {
-                    return true;
-                }
-            } else {
-                int max = (int) Math.sqrt(number);
-                for (int i = 2; i<=max; i +=2) {
-                    if(number %i == 0) {
-                        return false;
-                    }
-                    return true;
-                }
-            }
+        if(number < 2) {
+            return false;
         }
-        return false;
+
+        if(number == 2) {
+            return true;
+        }
+
+        for(int i=2;i<(number/2)+1;i++) {
+            if(number%i==0)
+                return false;
+        }
+        return true;
     }
-}
+    }
